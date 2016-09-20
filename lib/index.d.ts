46a4116713924e2cdaa27aa04bcb940d34a8225d
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:14:51 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import GaeaWebComponents from './gaea-web-components/gaea-web-components.component';
+import { PropsDefine as GaeaWebComponentsPropsDefine } from './gaea-web-components/gaea-web-components.type';
+export { GaeaWebComponents, GaeaWebComponentsPropsDefine };
+export default GaeaWebComponents;
