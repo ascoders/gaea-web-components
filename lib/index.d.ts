fa877068028b126fea3d27c5a8c0520e9b2ec7f0
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import GaeaWebComponents from './gaea-web-components/gaea-web-components.component';
-import {PropsDefine as GaeaWebComponentsPropsDefine} from './gaea-web-components/gaea-web-components.type';
-export {GaeaWebComponents, GaeaWebComponentsPropsDefine};
+import { PropsDefine as GaeaWebComponentsPropsDefine } from './gaea-web-components/gaea-web-components.type';
+export { GaeaWebComponents, GaeaWebComponentsPropsDefine };
 export default GaeaWebComponents;
