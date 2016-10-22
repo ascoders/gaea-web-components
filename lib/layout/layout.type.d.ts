c04db5dd608fb0f84be9acc578ee03ae7132cee8
huangziyi01 <huangziyi01@baidu.com>
2016-10-22 11:30:59 +0800

    发布组件

--- lib/layout/layout.type.d.ts
+++ lib/layout/layout.type.d.ts
@@ -6,6 +6,7 @@ export declare class PropsGaea {
     gaeaName: string;
     gaeaIcon: string;
     gaeaUniqueKey: string;
+    canDragIn: boolean;
     gaeaEdit: (string | {
         field: any;
         label: string;
