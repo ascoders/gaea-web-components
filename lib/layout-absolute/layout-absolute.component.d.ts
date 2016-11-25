1acdce110c9c6509a0e14d9bc01ceb390237459e
huangziyi01 <huangziyi01@baidu.com>
2016-11-25 21:54:59 +0800

    发布组件

--- lib/layout-absolute/layout-absolute.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as typings from './layout-absolute.type';
-export default class LayoutAbsoluteComponent extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
